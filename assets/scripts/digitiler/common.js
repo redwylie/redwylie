d2a7f932776bf0630ed4da826a16047bda8b6806
Mark Wylie <mwylie@partners.org>
2017-03-05 15:44:09 -0500

    adding back the digitiler large format stuff

--- assets/scripts/digitiler/common.js
+++ assets/scripts/digitiler/common.js
@@ -5,6 +5,21 @@ $("document").ready (function(){
 function setProps(){
 	var currentYear = new Date().getFullYear();
 	$("#year").text(currentYear);
+
+	// DIGITILER SPECIFIC
+	$('.hidden-info').hide();
+	$('.show-info').click(function() {
+		$('.hidden-info').toggle();
+	});
+
+	var width = $(window).width();
+	width = width - 30;
+  	$('#openseadragon').css({'width' : width})
+
+	var height = $(window).height();
+	height = height - 80;
+	$('#openseadragon').css({'height' : height})
+	// END DIGITILER SPECIFIC
 }
 
 function goBack() {
